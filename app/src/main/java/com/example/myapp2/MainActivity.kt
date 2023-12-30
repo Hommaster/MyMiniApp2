7b292d93fc4b4ed7c8023d71b8957e468ea3959d
Hommaster <ilya.pan.2017@mail.ru>
2023-12-30 22:04:44 +0300

    First step in second app

--- app/src/main/java/com/example/myapp2/MainActivity.kt
+++ app/src/main/java/com/example/myapp2/MainActivity.kt
@@ -2,15 +2,39 @@ package com.example.myapp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.example.myapp2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bindingClass: ActivityMainBinding
 
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
+        trueButton = bindingClass.trueButton
+        falseButton = bindingClass.falseButton
+
+        trueButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        falseButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
     }
 }
--- app/src/main/java/com/example/myapp2/Plant.kt
+++
@@ -1,3 +0,0 @@
-package com.example.myapp2
-
-data class Plant(var imageId: Int, var title: String)
