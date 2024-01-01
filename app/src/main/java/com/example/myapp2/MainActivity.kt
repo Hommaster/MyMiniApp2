1f5411f92a88b86a13828b38a460799ede765ba9
Hommaster <ilya.pan.2017@mail.ru>
2024-01-01 18:31:23 +0300

    First step in second app

--- app/src/main/java/com/example/myapp2/MainActivity.kt
+++ app/src/main/java/com/example/myapp2/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.myapp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.myapp2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
 
+    private val questionBank = listOf(
+        Questions(R.string.question_australia, true),
+        Questions(R.string.question_ocean, true),
+        Questions(R.string.question_mideast, false),
+        Questions(R.string.question_africa, false),
+        Questions(R.string.question_americas, true),
+        Questions(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
@@ -21,19 +35,21 @@ class MainActivity : AppCompatActivity() {
         falseButton = bindingClass.falseButton
 
         trueButton.setOnClickListener {
-            Toast.makeText(
+            val toast = Toast.makeText(
                 this,
                 R.string.correct_toast,
                 Toast.LENGTH_SHORT
-            ).show()
+            )
+            toast.show()
         }
 
         falseButton.setOnClickListener {
-            Toast.makeText(
+            val toast = Toast.makeText(
                 this,
                 R.string.incorrect_toast,
                 Toast.LENGTH_SHORT
-            ).show()
+            )
+            toast.show()
         }
 
     }
---
+++ app/src/main/java/com/example/myapp2/Questions.kt
@@ -0,0 +1,7 @@
+package com.example.myapp2
+
+import androidx.annotation.StringRes
+
+data class Questions(@StringRes val textId: Int, val answer: Boolean) {
+
+}
